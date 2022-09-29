5e812cd4d1a55037d107f4fff33262655884a054
José Vera <joseantonioveraisasis@gmail.com>
2022-09-29 12:25:21 -0300

    add3

--- deploy.php
+++ deploy.php
@@ -1,38 +1,44 @@
-<?php 
-    /** 
-     * GIT DEPLOYMENT SCRIPT 
-     * 
-     * Usado para implementar automáticamente sitios web a través de GitHub 
-     * 
-     */ 
+<?php
+    /**
+     * GIT DEPLOYMENT SCRIPT
+     *
+     * Used for automatically deploying websites via GitHub
+     *
+     */
 
-    // matriz de comandos 
-    $comandos = matriz( 
-        'sudo whoami', 
-        'sudo git pull', 
-        'sudo git status ', 
-        'sincronización de submódulo de git', 
-        'actualización de submódulo de git', 
-        'estado de submódulo de git', 
-    ); 
+    // array of commands
+    $commands = array(
+        'echo $PWD',
+        'whoami',
+        'sudo git pull',
+        'sudo git status',
+        'sudo git submodule sync',
+        'sudo git submodule update',
+        'sudo git submodule status',
+    );
 
-    // comandos exec 
-    $salida = ''; 
-    foreach($comandos AS $comando){ 
-        $tmp = "xxxxxxxxxxx";
+    // exec commands
+    $output = '';
+    foreach($commands AS $command){
+        $tmp = shell_exec($command);
         
-       
-        $salida .= htmlentities(trim($tmp)) . "\n<br /><br />"; 
-    } 
-?> 
+        $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
+        $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
+    }
+?>
 
-<!DOCTYPE HTML> 
-<html lang="en-US"> 
-<head> 
-    <meta charset="UTF-8"> 
-    <title>GUIÓN DE IMPLEMENTACIÓN GIT</title> 
-</head> 
-<body style=" color de fondo: #000000; color: #FFFFFF; grosor de fuente: negrita; relleno: 0 10px;"> 
-<div style="width:700px"> 
-    <div style="float:left;width:350px;"> 
-    <p estilo="color:blanco;
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>GIT DEPLOYMENT SCRIPT</title>
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<div style="width:700px">
+    <div style="float:left;width:350px;">
+    <p style="color:white;">Git Deployment Script</p>
+    <?php echo $output; ?>
+    </div>
+</div>
+</body>
+</html>
