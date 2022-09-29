29d8642bdc94730789a9d810929ee8c3aef7eafd
José Vera <joseantonioveraisasis@gmail.com>
2022-09-29 12:29:29 -0300

    add6

--- deploy.php
+++ deploy.php
@@ -10,11 +10,11 @@
     $commands = array(
         'echo $PWD',
         'whoami',
-        'sudo git pull',
-        'sudo git status',
-        'sudo git submodule sync',
-        'sudo git submodule update',
-        'sudo git submodule status',
+        'git pull',
+        'git status',
+        'git submodule sync',
+        'git submodule update',
+        'git submodule status',
     );
 
     // exec commands
