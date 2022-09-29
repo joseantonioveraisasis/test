32e8f3fcb3ca725139515224bf6c23e41370e3d4
José Vera <joseantonioveraisasis@gmail.com>
2022-09-29 15:27:42 -0300

    c1

--- deploy.php
+++ deploy.php
@@ -22,7 +22,7 @@
     foreach($commands AS $command){
         $tmp = shell_exec($command);
         
-        $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
+        $output .= "<span style=\"color: #32CD32;\">\$ </span><span style=\"color: #32CD32;\">{$command}\n</span><br />";
         $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
     }
 ?>
@@ -31,11 +31,11 @@
 <html lang="en-US">
 <head>
     <meta charset="UTF-8">
-    <title>GIT DEPLOYMENT SCRIPT</title>
+    <title>Git Deployment Script</title>
 </head>
 <body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
 <div style="width:700px">
-    <div style="float:left;width:350px;">
+    <div style="float:left;">
     <p style="color:white;">Git Deployment Script</p>
     <?php echo $output; ?>
     </div>
